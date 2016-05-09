3ff97453afc7221ef676d4c806be238150543cc6
Ricardo <ricardo.zanardo@gmail.com>
2016-05-09 20:42:03 -0300

    Novo service para criação de database.

---
+++ src/main/java/br/com/gotorcidaws/services/DatabaseService.java
@@ -0,0 +1,30 @@
+package br.com.gotorcidaws.services;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import br.com.gotorcida.fw.Message;
+import br.com.gotorcidaws.main.DatabaseGenerator;
+
+@Path("database")
+public class DatabaseService {
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public String create() {
+
+		Message message = new Message();
+		try {
+			DatabaseGenerator.createDatabase();
+			message.setSystem("code", "200");
+			message.setSystem("message", "Database criado com sucesso.");
+		} catch (Exception e) {
+			message.setSystem("code", "500");
+			message.setSystem("message", "Erro ao criar banco de dados.");
+			message.addData("errorMessage", e.getMessage());
+		}
+
+		return message.toJSON();
+	}
+}
